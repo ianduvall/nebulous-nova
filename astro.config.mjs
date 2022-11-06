6bdf12c28d6bce765011d7d71097736310ce96fb
Ian Duvall <iduvall@umich.edu>
2022-11-06 17:11:08 -0600

    Add cloudflare adaptor

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,10 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import cloudflare from "@astrojs/cloudflare";
 
-// https://astro.build/config
-export default defineConfig({});
+/**
+ * @type {import('astro').AstroUserConfig}
+ */
+export default defineConfig({
+	output: "server",
+	adapter: cloudflare(),
+});
